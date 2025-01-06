c968b493135558237b8fe32fbbf07e97d0dcab05
trandoanquanghieu <trandoanquanghieu@gmail.com>
2025-01-06 22:48:12 +0700

    Hoàn thành đăng ký cho người thuê trọ -> Tự điều hướng sang nguoiThueTroDashboardView

--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -3,30 +3,47 @@ package controller;
 import backend.Register;
 import frontend.view.RegisterView;
 import frontend.view.UpdateChutroInforView;
+import frontend.view.nguoiThueTroDashboardView;
 
 import javax.swing.*;
 
 public class RegisterController {
     public static void handleRegister(String username, String password, String confirmPassword, String role, JFrame frame) {
-        boolean result = Register.registerUser("chutro", username, password, confirmPassword);
-//        Debug line
+        boolean result = Register.registerUser(role, username, password, confirmPassword);
+//        Debug lineqQq
 //        boolean userCheckResult = Register.userNameCheck(username);
-//        System.out.println("Result user check: " + userCheckResult);
-
+//        System.out.println("Result user check: " + userCheckResult)
         System.out.println(result);
-        if (result) {
-            frame.dispose();
-            new UpdateChutroInforView(username); // Chuyển hướng đến UpdateChutroInforView
-        } else {
-            JOptionPane.showMessageDialog(frame, "Tên tài khoản đã tồn tại hoặc xác nhận mật khẩu không chính xác!", "Lỗi", JOptionPane.ERROR_MESSAGE);
-            JOptionPane.showMessageDialog(frame, "Đăng ký thất bại!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+
+        switch(role) {
+            case "chutro":
+                if (result) {
+                    JOptionPane.showMessageDialog(frame, "Bạn đã đăng ký thành công với vai trò chủ trọ!", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+                    frame.dispose();
+                    new UpdateChutroInforView(username); // Chuyển hướng đến UpdateChutroInforView
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Tên tài khoản đã tồn tại hoặc xác nhận mật khẩu không chính xác!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(frame, "Đăng ký thất bại!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+
+            case "nguoithuetro":
+                if (result) {
+                    JOptionPane.showMessageDialog(frame, "Bạn đã đăng ký thành công với vai trò người thuê trọ!", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+                    frame.dispose();
+                    new nguoiThueTroDashboardView();
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Đăng ký thất bại!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
         }
+
     }
 
     public static void updateChutroInfo(String fullName, String cccd, String phone, String username, JFrame frame) {
 //        System.out.println(username);
         String userID = Register.getUserIDFromUsers(username);
-        System.out.println("UserID: " + userID);
+//        System.out.println("UserID: " + userID);
         boolean result = Register.updateChutroInfo(userID, fullName, cccd, phone);
 
         if (result) {
