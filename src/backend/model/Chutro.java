0a77afa7ccb53713759445ba30d800cdbe03ee13
trandoanquanghieu <trandoanquanghieu@gmail.com>
2025-01-13 23:04:36 +0700

    Fix bugs

--- src/backend/model/Chutro.java
+++ src/backend/model/Chutro.java
@@ -277,6 +277,21 @@ public class Chutro {
         }
         return null;
     }
+
+    public static int getIDChutroFromHoTen(String hoten){
+        try(Connection conn = connectDatabase.DatabaseConnection.getConnection()) {
+            String sql = "SELECT IDChutro FROM Chutro WHERE HoTen = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, hoten);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("IDChutro");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
     /// STRIKE TODO: Xử lý lấy thông tin tên chủ trọ và tổng số phòng chủ trụ nắm giữ
     public static int getCountRoomFromIdChutro(int idChutro){
         try (Connection conn = connectDatabase.DatabaseConnection.getConnection()) {
