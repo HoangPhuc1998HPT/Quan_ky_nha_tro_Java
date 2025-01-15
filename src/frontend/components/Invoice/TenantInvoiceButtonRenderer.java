1d54d40b409ab5ace867243326f1b4b05cbf525c
Hoangphuctran1998hpt <139626775+HoangPhuc1998HPT@users.noreply.github.com>
2025-01-15 15:07:55 +0700

    update NguoiThueTroDashboard_1View

--- src/frontend/components/Invoice/TenantInvoiceButtonRenderer.java
+++ src/frontend/components/Invoice/TenantInvoiceButtonRenderer.java
@@ -11,7 +11,19 @@ public class TenantInvoiceButtonRenderer extends JButton implements TableCellRen
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        // Đặt văn bản cho nút
         setText(value == null ? "Xem" : value.toString());
+
+        // Thay đổi màu nền và màu chữ
+        setBackground(new Color(81, 129, 216)); // Màu xanh nhạt
+        setForeground(Color.BLACK); // Màu chữ đen
+
+        // Đổi màu khi hàng được chọn
+        if (isSelected) {
+            setBackground(new Color(36, 101, 219)); // Màu xanh đậm hơn
+            setForeground(Color.WHITE); // Màu chữ trắng
+        }
+
         return this;
     }
 }
